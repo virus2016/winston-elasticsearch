3803bd7a14bb4821b3b9a67c2aab0bed4606318f
Calum MacRae <cm@mipass.me>
2018-08-19 15:21:35 +0100

    Update index.d.ts

--- index.d.ts
+++ index.d.ts
@@ -1,41 +1,45 @@
-declare module 'winston-elasticsearch' {
-	import TransportStream from 'winston-transport';
-	import elasticsearch from 'elasticsearch';
+import * as  TransportStream from 'winston-transport';
+import * as elasticsearch from 'elasticsearch';
 
-	export interface LogData {
-		message: any;
-		level: string;
-		meta: { [key: string]: any };
-		timestamp?: string;
-	}
+declare namespace WinstonElasticsearch {
 
-	export interface Transformer {
-		(logData: LogData): any;
-	}
+  export interface LogData {
+    message: any;
+    level: string;
+    meta: { [key: string]: any };
+    timestamp?: string;
+  }
 
-	export interface ElasticsearchTransportOptions extends TransportStream.TransportStreamOptions {
-		timestamp?: () => string;
-		level?: string;
-		index?: string;
-		indexPrefix?: string;
-		indexSuffixPattern?: string;
-		messageType?: string;
-		transformer?: Transformer;
-		mappingTemplate?: { [key: string]: any };
-		ensureMappingTemplate?: boolean;
-		flushInterval?: number;
-		waitForActiveShards?: number;
-		handleExceptions?: boolean;
-		pipeline?: string;
-		client?: elasticsearch.Client;
-		clientOpts?: elasticsearch.ConfigOptions;
-	}
+  export interface Transformer {
+    (logData: LogData): any;
+  }
 
-	export default class Elasticsearch extends TransportStream {
-		constructor(opts?: ElasticsearchTransportOptions);
+  export interface ElasticsearchTransportOptions extends TransportStream.TransportStreamOptions {
+    timestamp?: () => string;
+    level?: string;
+    index?: string;
+    indexPrefix?: string;
+    indexSuffixPattern?: string;
+    messageType?: string;
+    transformer?: Transformer;
+    mappingTemplate?: { [key: string]: any };
+    ensureMappingTemplate?: boolean;
+    flushInterval?: number;
+    waitForActiveShards?: number;
+    handleExceptions?: boolean;
+    pipeline?: string;
+    client?: elasticsearch.Client;
+    clientOpts?: elasticsearch.ConfigOptions;
+  }
 
-		query<T>(options: any, callback?: () => void): Promise<elasticsearch.SearchResponse<T>>;
-		query<T>(q: string): Promise<elasticsearch.SearchResponse<T>>;
-		getIndexName(opts: ElasticsearchTransportOptions): string;
-	}
+  export interface Elasticsearch extends TransportStream {
+    new(opts?: ElasticsearchTransportOptions);
+
+    query<T>(options: any, callback?: () => void): Promise<elasticsearch.SearchResponse<T>>;
+    query<T>(q: string): Promise<elasticsearch.SearchResponse<T>>;
+    getIndexName(opts: ElasticsearchTransportOptions): string;
+  }
 }
+
+declare const ElasticsearchWinston: WinstonElasticsearch.Elasticsearch;
+export = ElasticsearchWinston;
